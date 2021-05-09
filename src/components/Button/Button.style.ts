5fb5f09b01840fdd0b1c805d0c0af24e01c22fe1
hoolek77 <szymon.kin@gmail.com>
2021-05-09 13:04:23 +0200

    feat: add reusable button component

---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components';
+
+export enum ButtonStyle {
+  SIGN_MAIN = 'sign-main',
+  SIGN_SECONDARY = 'sign-secondary',
+}
+
+const signMain = css`
+  width: 180px;
+  height: 35px;
+  font-size: 1.15rem;
+  background-color: ${(props) => props.theme.primary};
+  border: none;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const signSecondary = css`
+  width: 180px;
+  height: 35px;
+  font-size: 1rem;
+  background-color: ${(props) => props.theme.customWhite};
+  border: 2px solid ${(props) => props.theme.primary};
+  border-radius: 10px;
+  &:hover {
+    transform: scale(1.05);
+    background-color: ${(props) => props.theme.primary};
+    color: #ffffff;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const getButtonStyles = ({ buttonStyle }: { buttonStyle: ButtonStyle }) => {
+  switch (buttonStyle) {
+    case ButtonStyle.SIGN_MAIN:
+      return signMain;
+    case ButtonStyle.SIGN_SECONDARY:
+      return signSecondary;
+    default:
+      return null;
+  }
+};
+
+export const CustomButton = styled.button`
+  outline: none;
+  cursor: pointer;
+  transition-duration: 200ms;
+  &:disabled {
+    filter: brightness(0.85);
+  }
+  ${getButtonStyles};
+`;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+import { ButtonStyle, CustomButton } from './Button.style';
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  children: ReactNode;
+  buttonStyle: ButtonStyle;
+}
+
+const Button = (props: ButtonProps) => {
+  const { buttonStyle, children, ...rest } = props;
+  return (
+    <CustomButton buttonStyle={buttonStyle} {...rest}>
+      {children}
+    </CustomButton>
+  );
+};
+
+export { Button };
