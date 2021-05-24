0a328ac7d750cd4ee3e247b29ea980fb369e69c8
hoolek77 <szymon.kin@gmail.com>
2021-05-24 13:06:39 +0200

    fix: initialize i18n translation correctly based on local storage data

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,19 @@
 import { initReactI18next } from 'react-i18next';
+import { LS_LANGUAGE } from 'constants/language';
 import i18n from 'i18next';
+import { isLanguage } from 'utils';
 
 import { en } from './en';
 import { pl } from './pl';
+
+const LANGUAGE = localStorage.getItem(LS_LANGUAGE);
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     pl: { translation: pl },
   },
-  lng: 'en',
+  lng: isLanguage(LANGUAGE) ? LANGUAGE : 'en',
   fallbackLng: 'en',
   interpolation: { escapeValue: false },
 });
